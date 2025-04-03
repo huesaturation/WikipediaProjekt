64eff295525151d360612f9eb487612ca05ae64f
huesaturation <malte@drischel.de>
2025-04-03 14:52:40 +0200

    Basic GUI Setup

---
+++ src/GUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class GUI
+{
+    JFrame window = new JFrame("Prime Facts");
+
+    public GUI()
+    {
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setSize(1920 , 1080);
+        window.setVisible(true);
+    }
+
+}
