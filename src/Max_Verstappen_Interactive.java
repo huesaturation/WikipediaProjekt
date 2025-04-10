1645485e5bc5682f7f01fb6d61de4c4db006f08b
Jakob <pilz.jakob62@gmail.com>
2025-04-10 20:56:14 +0200

    Added Page layout

---
+++ src/Max_Verstappen_Interactive.java
@@ -0,0 +1,33 @@
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+import javax.swing.JOptionPane;
+
+public class Max_Verstappen_Interactive {
+    public static void main(String[] args) {
+    String filepath = "Verstappen.wav";
+    PlayMusic(filepath);
+    JOptionPane.showMessageDialog(null,"Press Ok to stop playing");
+    }
+    public static void PlayMusic(String location)
+    {
+        try{
+            File musicPath = new File(location);
+            if (musicPath.exists()){
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInput);
+                clip.start();
+            }
+            else{
+                System.out.println("Can`t find file");
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+}
